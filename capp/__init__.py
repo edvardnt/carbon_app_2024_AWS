e40a91b6010c8fb6b8a79b64409a97a5a9723a60
benjaminlienL <160712548+benjaminlienL@users.noreply.github.com>
2024-04-09 16:26:45 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
-
+from flask_login import LoginManager
 application = Flask(__name__)
 
-application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'  
 
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager = LoginManager(application)
+login_manager.login_view='users.login'
+login_manager.login_message_category='info'
 
 from capp.carbon_app.routes import carbon_app
 from capp.methodology.routes import methodology
