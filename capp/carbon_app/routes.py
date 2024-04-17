d2d16330cbb438ab5b78b379984e55f73940443c
eberents2 <160642520+eberents2@users.noreply.github.com>
2024-04-17 09:57:03 +0200

    Update routes.py

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -12,7 +12,7 @@ carbon_app = Blueprint('carbon_app', __name__)
 efco2 = {
     'Bus': {'Diesel': 0.025, 'Electric': 0.013, 'Biodiesel': 0.007},
     'Car': {'Old Petrol': 0.156, 'New Petrol': 0.139, 'Old Diesel': 0.130, 'New Diesel': 0.122, 'Small Electric': 0.012, 'Medium Electric': 0.016, 'Large Electric': 0.020},
-    'Plane': {'Long-haul Economy Class': 0.102, 'Long-haul Business Class':0.306, 'Long-haul First Class':0.408, 'Short-haul Economy Class':0.133,'Short-haul Business Class':0.399, 'Short-haul First Class':0.532},
+    'Plane': {'Long-haul Economy Class': 0.102, 'Long-haul Business class':0.306, 'Long-haul First class':0.408, 'Short-haul Economy class':0.133,'Short-haul Business class':0.399, 'Short-haul First class':0.532},
     'Ferry': {'Diesel': 0.226, 'Electric': 0.00525},
     'Motorbike': {'Petrol': 0.114},
     'Train':{'Diesel':0.091, 'Electric Nordic':0.007 , 'Electric European': 0.024},
